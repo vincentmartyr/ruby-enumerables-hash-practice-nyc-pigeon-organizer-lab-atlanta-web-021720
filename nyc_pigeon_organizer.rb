2448ebbb9bda8bcec856c15b0f1a55ebcaa3a4b8
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-16 00:12:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,38 @@
+require 'pp'
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+pigeon_names = {}
+
+data[:gender].each{|key, value|
+  value.each{ |name|
+pigeon_names[name] = {
+  :gender => [key.to_s],
+  :color => [],
+  :lives => []
+}
+data[:color].each{|key, value|
+ value.each{ |color|
+pigeon_names[color]
+  :color[key]    }
+}
+}
+# data[:color].each{|key, value|
+#   value.each{ |color|
+#     pigeon_names[color] =
+#     :color = [key]    }
+binding.pry
+
+}
+
+
+
+
+# result
+
 end
+
+
+
+#  for each uniq name in source start an hash
+#  inside each hash create keys for :color :gender :lives
+#  for each key create array
