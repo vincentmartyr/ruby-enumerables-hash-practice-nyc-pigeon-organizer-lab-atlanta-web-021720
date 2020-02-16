77d909e59b6b6c47d3779c4a5e5b71f7c031bd68
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-16 02:06:28 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
+require 'pp'
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+pigeon_names = {}
+
+data[:gender].each{|key, value|
+  value.each{ |name|
+pigeon_names[name] = {
+  :gender => [key.to_s],
+  :color => [],
+  :lives => []}
+
+}
+}
+data[:color].each{|key, value|
+ value.each { |name|
+pigeon_names[name][:color] << key.to_s }
+
+}
+
+data[:lives].each{|key, value|
+value.each { |name|
+  pigeon_names[name][:lives] << key.to_s }
+
+}
+
+
+pigeon_names
 end
