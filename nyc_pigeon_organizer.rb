21e5cbce50261485c241d3d6b765ba9b104a9d4d
Vincent Martyr <vincentmartyr@gmail.com>
2020-02-16 00:27:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,14 +9,14 @@ pigeon_names[name] = {
   :gender => [key.to_s],
   :color => [],
   :lives => []
-
+}
 data[:color].each{|key, value|
  value.each{ |color|
 pigeon_names[color]}
 }
 }
 }
-}
+
 # data[:color].each{|key, value|
 #   value.each{ |color|
 #     pigeon_names[color] =
